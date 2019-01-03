bee058ec27fd6c6df74b0d42b361e86830faf051
Brian Shef <brian.shef@rewardstyle.com>
2019-01-03 15:06:20 -0600

    Differentiate ALphaBlock from Block

--- gemtype/block.py
+++ gemtype/block.py
@@ -3,31 +3,35 @@ from random import choice
 from string import ascii_uppercase
 
 FONT_SIZE = 32
-TYPE = ['default', 'char', 'word']
 
 class Block:
-    def __init__(self, size, margin, row, column, type=0):
+    def __init__(self, size, margin, row, column):
         self.size = size
         self.margin = margin
         self.row = row
         self.column = column
-        self.type = TYPE[type]
         self.x = (self.margin + self.size) * self.column + self.margin + self.size // 2
         self.y = (self.margin + self.size) * self.row + self.margin + self.size // 2
         self.center_x = self.x + self.size // 2
-        self.center_y = self.y + self.size // 2
-        self.text = '' if self.type == TYPE[0] else choice(ascii_uppercase)
+        self.center_y = self.y + self.size // 2        
     
     def draw(self):
-        draw_text = False
         color = (100, 120, 100, 50)
-        if self.type == TYPE[1]:
-            color = arcade.color.GREEN
-            draw_text=True
         arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, color)
-        if draw_text:
-            arcade.draw_text(self.text,
-                         self.x - self.size // 3.5, self.center_y, arcade.color.WHITE, FONT_SIZE,
+
+
+
+class AlphaBlock(Block):
+    def __init__(self, size, margin, row, column):
+        Block.__init__(self, size, margin, row, column)
+        self.text = choice(ascii_uppercase)
+
+    def draw(self):
+        color = arcade.color.GREEN            
+        arcade.draw_rectangle_filled(self.x, self.y, self.size, self.size, color)
+        arcade.draw_text(self.text,
+                         self.x - self.size // 3.5,
+                         self.center_y,
+                         arcade.color.WHITE,
+                         FONT_SIZE,
                          align="left", anchor_x="left", anchor_y="top")
-                         
-        
--- gemtype/grid.py
+++ gemtype/grid.py
@@ -2,6 +2,9 @@ import arcade
 from random import randint
 from . import block
 
+ALPHA_CHANCE = 10
+MIN_ROW_ALPHA = 5
+
 class Grid:
     def __init__(self, cell_width=50, cell_height=50, margin=1, screen_width=800, screen_height=600):
         self.cell_width = cell_width
@@ -13,10 +16,18 @@ class Grid:
         for row in range(self.row_count):
             self.grid.append([])
             for column in range(self.column_count):
-                b = block.Block(self.cell_width, self.margin, row, column, type=randint(0, 1))
+                b = generate_block(self.cell_width, self.margin, row, column)
                 self.grid[row].append(b)
     
     def draw(self):
         for row in range(self.row_count):
             for column in range(self.column_count):
                 self.grid[row][column].draw()
+
+
+
+def generate_block(size, margin, row, column):
+    t = randint(0, 99)
+    if t < ALPHA_CHANCE and row >= MIN_ROW_ALPHA:
+        return block.AlphaBlock(size, margin, row, column)
+    return block.Block(size, margin, row, column)
